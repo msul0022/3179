be6ee5269be7548bdfd9f714b5be6189970f4e96
msul0022 <msul0022@student.monash.edu>
2024-10-04 09:29:34 +1000

    Update vega_lite_vis.js

--- Week 10 HW/vega_lite_vis.js
+++ Week 10 HW/vega_lite_vis.js
@@ -1,4 +1,4 @@
-var vg_2 = "industry_income_bar.vg.json";
+var a = "industry_income_bar.vg.json";
 vegaEmbed("#bar", vg_2).then(function(result)                                     
                                     {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
