1284ce871e956437cf8da07aa659151dc5ccc84b
msul0022 <msul0022@student.monash.edu>
2024-10-03 17:31:54 +1000

    Create vega_lite_vis.js

---
+++ Week 10 HW/vega_lite_vis.js
@@ -0,0 +1,7 @@
+var vg_1 = "aus_income_choropleth.vg.json";
+vegaEmbed("#choropleth", vg_1).then(function(result) 
+var vg_2 = "industry_income_bar.vg.json";
+vegaEmbed("#bar", vg_2).then(function(result)                                     
+                                    {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
